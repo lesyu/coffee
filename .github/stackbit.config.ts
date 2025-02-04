72adb817facb560c175a18124edc49776832e687
lesyu <lesshing@gmail.com>
2025-02-04 17:39:21 +0800

    Update stackbit.config.ts
    20250204

--- .github/stackbit.config.ts
+++ .github/stackbit.config.ts
@@ -1,16 +1,4 @@
 // stackbit.config.ts
-import { defineStackbitConfig } from "@stackbit/types";
-
-export default defineStackbitConfig({
-  // ...
-  modelExtensions: [
-    // Extend the "Page" and "Post" models by defining them as page models
-    { name: "Page", type: "page" },
-    { name: "Post", type: "page" }
-  ]
-});
-
-// stackbit.config.ts
 import { defineStackbitConfig, SiteMapEntry } from "@stackbit/types";
 
 export default defineStackbitConfig({
