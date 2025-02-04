03fb28555ca8d6190d96d178e26c012867e0e9a6
lesyu <lesshing@gmail.com>
2025-02-04 17:19:06 +0800

    Add files via upload

--- .github/stackbit.config.ts
+++ .github/stackbit.config.ts
@@ -1,28 +1,58 @@
-import { defineStackbitConfig } from '@stackbit/types';
+import { defineStackbitConfig, DocumentStringLikeFieldNonLocalized, SiteMapEntry } from '@stackbit/types';
 import { GitContentSource } from '@stackbit/cms-git';
-import { allModels } from './.stackbit/models';
+import { allModels } from 'sources/local/models';
 
-const config = defineStackbitConfig({
-    stackbitVersion: '~0.6.0',
+const gitContentSource = new GitContentSource({
+    rootPath: __dirname,
+    contentDirs: ['content'],
+    models: Object.values(allModels),
+    assetsConfig: {
+        referenceType: 'static',
+        staticDir: 'public',
+        uploadDir: 'images',
+        publicPath: '/'
+    }
+});
+
+export const config = defineStackbitConfig({
+    stackbitVersion: '~0.7.0',
     ssgName: 'nextjs',
     nodeVersion: '18',
-    contentSources: [
-        new GitContentSource({
-            rootPath: __dirname,
-            contentDirs: ['content'],
-            models: allModels,
-            assetsConfig: {
-                referenceType: 'static',
-                staticDir: 'public',
-                uploadDir: 'images',
-                publicPath: '/'
-            }
-        })
-    ],
+    styleObjectModelName: 'ThemeStyle',
+    contentSources: [gitContentSource],
     presetSource: {
         type: 'files',
-        presetDirs: ['./.stackbit/presets']
+        presetDirs: ['sources/local/presets']
     },
-    styleObjectModelName: 'ThemeStyle'
+    siteMap: ({ documents, models }): SiteMapEntry[] => {
+        const pageModels = models.filter((model) => model.type === 'page').map((model) => model.name);
+        return documents
+            .filter((document) => pageModels.includes(document.modelName))
+            .map((document) => {
+                let slug = (document.fields.slug as DocumentStringLikeFieldNonLocalized)?.value;
+                if (!slug) return null;
+                /* Remove the leading slash in order to generate correct urlPath
+                regardless of whether the slug is '/', 'slug' or '/slug' */
+                slug = slug.replace(/^\/+/, '');
+                switch (document.modelName) {
+                    case 'PostFeedLayout':
+                        return {
+                            urlPath: '/blog',
+                            document: document
+                        };
+                    case 'PostLayout':
+                        return {
+                            urlPath: `/blog/${slug}`,
+                            document: document
+                        };
+                    default:
+                        return {
+                            urlPath: `/${slug}`,
+                            document: document
+                        };
+                }
+            });
+    }
 });
+
 export default config;
