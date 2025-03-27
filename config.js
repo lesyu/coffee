fc74e1318e039e40efb633237bca43826c721d5a
lesyu <lesshing@gmail.com>
2025-03-27 15:08:01 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+export const config = {
+    supabaseUrl: "https://frugoxerstqqlzuuavze.supabase.co",
+    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZydWdveGVyc3RxcWx6dXVhdnplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2NTUwMTAsImV4cCI6MjA1NDIzMTAxMH0.-WtJTxHfIrCAl_hYYnHcq45vNDD2Kf2pWkDv5NuVeOc" // Replace with your actual key
+}; 
